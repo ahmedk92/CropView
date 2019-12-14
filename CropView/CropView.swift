ac96b05f45d756e8e947065328a73cb31fab95c8
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2019-12-14 23:04:46 +0200

    add resize handles

--- CropView/CropView.swift
+++ CropView/CropView.swift
@@ -26,7 +26,60 @@ class CropView: UIView {
     private var holeRect = CGRect(x: 0, y: 0, width: 100, height: 100) {
         didSet {
             applyMask()
+            positionResizeViews()
         }
+    }
+    
+    private lazy var topLeftResizeView: ResizeView = {
+        let v = ResizeView(orientation: .topLeft, positionChangeHandler: { [weak self] (change) in
+            guard let self = self else { return }
+            
+            let newRect = CGRect(minX: self.holeRect.minX + change.x, minY: self.holeRect.minY + change.y, maxX: self.holeRect.maxX, maxY: self.holeRect.maxY)
+            guard newRect.minX < self.holeRect.maxX && newRect.minY < self.holeRect.maxY else { return }
+            self.holeRect = newRect
+        })
+        addSubview(v)
+        return v
+    }()
+    private lazy var topRightResizeView: ResizeView = {
+        let v = ResizeView(orientation: .topRight, positionChangeHandler: { [weak self] (change) in
+            guard let self = self else { return }
+            
+            let newRect = CGRect(minX: self.holeRect.minX, minY: self.holeRect.minY + change.y, maxX: self.holeRect.maxX + change.x, maxY: self.holeRect.maxY)
+            guard newRect.maxX > self.holeRect.minX && newRect.minY < self.holeRect.maxY else { return }
+            self.holeRect = newRect
+        })
+        addSubview(v)
+        return v
+    }()
+    private lazy var bottomLeftResizeView: ResizeView = {
+        let v = ResizeView(orientation: .bottomLeft, positionChangeHandler: { [weak self] (change) in
+            guard let self = self else { return }
+            
+            let newRect = CGRect(minX: self.holeRect.minX + change.x, minY: self.holeRect.minY, maxX: self.holeRect.maxX, maxY: self.holeRect.maxY + change.y)
+            guard newRect.minX < self.holeRect.maxX && newRect.maxY > self.holeRect.minY else { return }
+            self.holeRect = newRect
+        })
+        addSubview(v)
+        return v
+    }()
+    private lazy var bottomRightResizeView: ResizeView = {
+        let v = ResizeView(orientation: .bottomRight, positionChangeHandler: { [weak self] (change) in
+            guard let self = self else { return }
+            
+            let newRect = CGRect(minX: self.holeRect.minX, minY: self.holeRect.minY, maxX: self.holeRect.maxX + change.x, maxY: self.holeRect.maxY + change.y)
+            guard newRect.maxX > self.holeRect.minX && newRect.maxY > self.holeRect.minY else { return }
+            self.holeRect = newRect
+        })
+        addSubview(v)
+        return v
+    }()
+
+    private func positionResizeViews() {
+        topLeftResizeView.frame = CGRect(origin: holeRect.origin, size: CGSize(width: 16, height: 16))
+        topRightResizeView.frame = CGRect(origin: CGPoint(x: holeRect.maxX - ResizeView.lineWidth * 2, y: holeRect.minY), size: CGSize(width: 16, height: 16))
+        bottomLeftResizeView.frame = CGRect(origin: CGPoint(x: holeRect.minX, y: holeRect.maxY - ResizeView.lineWidth * 2), size: CGSize(width: 16, height: 16))
+        bottomRightResizeView.frame = CGRect(origin: CGPoint(x: holeRect.maxX - ResizeView.lineWidth * 2, y: holeRect.maxY - ResizeView.lineWidth * 2), size: CGSize(width: 16, height: 16))
     }
     
     private func applyMask() {
@@ -82,5 +135,78 @@ class CropView: UIView {
         if dimView.layer.mask == nil {
             applyMask()
         }
+    }
+}
+
+extension CropView {
+    class ResizeView: UIView {
+        
+        enum Orientation {
+            case topLeft, topRight, bottomLeft, bottomRight
+            
+            var transform: CGAffineTransform {
+                switch self {
+                case .topLeft: return .identity
+                case .topRight: return CGAffineTransform(scaleX: -1, y: 1)
+                case .bottomLeft: return CGAffineTransform(scaleX: 1, y: -1)
+                case .bottomRight: return CGAffineTransform(scaleX: -1, y: -1)
+                }
+            }
+        }
+        
+        @objc private func handlePanGesture(_ recognizer: UIPanGestureRecognizer) {
+            positionChangeHandler(recognizer.translation(in: superview))
+            recognizer.setTranslation(.zero, in: superview)
+        }
+        
+        typealias PositionChangeHandler = (CGPoint) -> Void
+        private let positionChangeHandler: PositionChangeHandler
+        
+        static let lineWidth: CGFloat = 8
+        
+        init(orientation: Orientation, positionChangeHandler: @escaping PositionChangeHandler) {
+            self.positionChangeHandler = positionChangeHandler
+            super.init(frame: .zero)
+            isOpaque = false
+            backgroundColor = .clear
+            addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:))))
+            transform = orientation.transform
+        }
+        
+        override init(frame: CGRect) {
+            fatalError()
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        override func draw(_ rect: CGRect) {
+            guard let context = UIGraphicsGetCurrentContext() else { return }
+                        
+            context.setFillColor(UIColor.clear.cgColor)
+            context.fill(bounds)
+            
+            let path = UIBezierPath()
+            let lineWidth = ResizeView.lineWidth
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: bounds.maxX, y: 0))
+            path.addLine(to: CGPoint(x: bounds.maxX, y: lineWidth))
+            path.addLine(to: CGPoint(x: lineWidth, y: lineWidth))
+            path.addLine(to: CGPoint(x: lineWidth, y: bounds.maxY))
+            path.addLine(to: CGPoint(x: 0, y: bounds.maxY))
+            path.addLine(to: CGPoint(x: 0, y: 0))
+            path.close()
+            
+            context.addPath(path.cgPath)
+            context.setFillColor(UIColor(white: 1, alpha: 0.9).cgColor)
+            context.fillPath()
+        }
+    }
+}
+
+extension CGRect {
+    init(minX: CGFloat, minY: CGFloat, maxX: CGFloat, maxY: CGFloat) {
+        self.init(x: minX, y: minY, width: maxX - minX, height: maxY - minY)
     }
 }
