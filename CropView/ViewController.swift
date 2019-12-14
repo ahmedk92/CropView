f4248191cde512977f6089b6c0f12ca18c9cd40c
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2019-12-14 13:19:50 +0200

    Initial Commit

---
+++ CropView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CropView
+//
+//  Created by Ahmed Khalaf on 12/14/19.
+//  Copyright © 2019 Ahmed Khalaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
